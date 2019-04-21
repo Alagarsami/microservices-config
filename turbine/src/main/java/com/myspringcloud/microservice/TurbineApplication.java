98fd058e8d141e394b21fe5e49205bdda06bb0f6
Alagarsami <alagarsami@gmail.com>
2019-04-21 13:15:05 +0800

    Add files via upload

---
+++ turbine/src/main/java/com/myspringcloud/microservice/TurbineApplication.java
@@ -0,0 +1,19 @@
+package com.myspringcloud.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.amqp.EnableTurbineAmqp;
+
+
+@SpringBootApplication
+@EnableTurbineAmqp	
+@EnableEurekaClient
+public class TurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
+
+}
+  
